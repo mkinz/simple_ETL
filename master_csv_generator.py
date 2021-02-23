b374c852a4b06d6068ae01d5b481d2cc32109840
Matthew Kinzler <matthew.kinzler@gmail.com>
2021-02-23 16:14:53 -0500

    cmd line interface is working

--- master_csv_generator.py
+++ master_csv_generator.py
@@ -2,6 +2,7 @@ import re
 import pandas as pd
 import petl as etl
 import os
+import sys
 import glob
 
 class XLabData:
@@ -70,14 +71,47 @@ class CSVMerger:
         finaldf = pd.concat(dfs, axis=1, join='outer').to_csv(destination + "\\master.csv")
         return finaldf
 
-source = 'C:\\Users\\matth\\Downloads\\dae-challenge\\dae-challenge\\x-lab-data'
+
+'''source = 'C:\\Users\\matth\\Downloads\\dae-challenge\\dae-challenge\\x-lab-data'
 destination = 'C:\\Users\\matth\\Downloads\\dae-challenge\\dae-challenge\\x-lab-data'
 
 xlab_data_writer = XLabData().write_new_xlab_csv_files(source, destination)
-merger = CSVMerger().merge_csv(source, destination)
+merger = CSVMerger().merge_csv(source, destination)'''
 
 
 class Runner:
-    def run_app(self):
-        pass
+    def cmd_line_interface(self):
+        print("Welcome to the Data Management System application!")
+        while True:
+            print("Please enter the source path now:\n")
+            source = input()
+            print("Please enter the destination (path to save files) now:\n")
+            destination = input()
+            print(f"Your source path is: \n{source}\n\n and destination path is: \n{destination}\n")
+            print("Is this correct? Type [y]es or [n]o.")
+            answer = input()
+            answers = ["yes", "Yes", 'YES', 'y', 'Y']
+            if answer in answers:
+                print('What would you like to do?')
+                print("1. Generate new master CSV from existing files in source")
+                print("2: Load new Xlab data and generate a new master CSV")
+                print("3: Quit")
+                options = int(input())
+                if options == 1:
+                    myMerger = CSVMerger()
+                    myMerger.merge_csv(source, destination)
+                    break
+                elif options == 2:
+                    myXlab_data_writer = XLabData()
+                    myXlab_data_writer.write_new_xlab_csv_files(source, destination)
+                    myMerger = CSVMerger()
+                    myMerger.merge_csv(source, destination)
+                    break
+                elif options == 3:
+                    sys.exit(0)
+                else:
+                    pass
 
+        sys.exit(0)
+
+myrunner = Runner().cmd_line_interface()
