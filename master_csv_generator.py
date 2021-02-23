3d75999ba4d2ae3c4c9ae4964f455ae14703d66a
Matthew Kinzler <matthew.kinzler@gmail.com>
2021-02-23 13:00:43 -0500

    refactoring

--- master_csv_generator.py
+++ master_csv_generator.py
@@ -19,64 +19,49 @@ def find_files(path):
     return files_to_read
 
 
-### grab first file contents for header
-def find_first_col_for_header():
-    path = get_path()
-    my_files = find_files(path)
-    regex = re.compile('(.*)\t.*')
+class XLabData:
+    def set_up_headers(self):
+        path = get_path()
+        my_files = find_files(path)
+        regex = re.compile('(.*)\t.*')
 
-    found_stuff = []
-    # use first file in files_to_read to get headers
-    with open(my_files[0], 'r') as f:
-        mydata = f.read()
-        for col1 in re.findall(regex, mydata):
-            found_stuff.append((col1))
-    return found_stuff
-
-
-### iterate over all files to get data from second column
-def find_second_col_for_data():
-    path = get_path()
-    my_files = find_files(path)
-    regex = re.compile('.*\t(.*)')
-
-    data_headers = find_first_col_for_header()
-    df = pd.DataFrame(columns=data_headers)
-    # print(df)
-
-    for file in my_files:
-        list_of_found_stuff = []
-        with open(file, 'r') as f:
+        found_stuff = []
+        # use first file in files_to_read to get headers
+        with open(my_files[0], 'r') as f:
             mydata = f.read()
-            for col2 in re.findall(regex, mydata):
-                list_of_found_stuff.append((col2))
-            df.loc[file] = list_of_found_stuff
-    return df
+            for col1 in re.findall(regex, mydata):
+                found_stuff.append((col1))
+        return found_stuff
 
 
-###create dataframe header
-# data_headers = find_first_col_for_header()
-# print(data_headers)
-# df = pd.DataFrame(data_headers,columns=['Item']).set_index('Item').T
-# df = pd.DataFrame(columns=data_headers)
-# df.drop(df.columns[[1]], axis=1, index=True)
+    ### iterate over all files to get data from second column
+    def build_xlab_dataframe(self):
+        path = get_path()
+        my_files = find_files(path)
+        regex = re.compile('.*\t(.*)')
 
-# print(df)
-# df.to_csv("df_headertest.csv")
-# table = etl.fromdataframe(df)
-# print(table)
-# table.tocsv("etl_headertest.csv")
-# data = find_second_col_for_data()
-# print(data)
-# data_csv = data.to_csv("test.csv")
+        data_headers = XLabData().set_up_headers()
+        df = pd.DataFrame(columns=data_headers)
+        # print(df)
 
-data = find_second_col_for_data()
+        for file in my_files:
+            list_of_found_stuff = []
+            with open(file, 'r') as f:
+                mydata = f.read()
+                for col2 in re.findall(regex, mydata):
+                    list_of_found_stuff.append((col2))
+                df.loc[file] = list_of_found_stuff
+        return df
+
+
+
+data = XLabData().build_xlab_dataframe()
 
 table = etl.fromdataframe(data)
 print(table)
 
 ###write it to
-etl.tocsv(table, "icp_test.csv")
+#etl.tocsv(table, "icp_test.csv")
 
 
 # This is for merging dataframes together
