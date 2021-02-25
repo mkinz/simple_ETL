f5baaf8d4a1856adffc5e8061bdb7c6db31e7dc2
Matthew Kinzler <matthew.kinzler@gmail.com>
2021-02-25 10:31:09 -0500

    applying PEP8 guidelines via the interpreter

--- xmat_simple_dms.py
+++ xmat_simple_dms.py
@@ -1,3 +1,5 @@
+# 02/25/2021 Matthew Kinzler for Citrine Informatics
+
 import re
 import os
 import sys
@@ -5,20 +7,21 @@ import glob
 import pandas as pd
 import petl as etl
 
-class CSVMerger:
 
+class Merger:
     """Merger class with single class method
     for merging all the CSV files together.
     It has built in protection to ensure that we
     do not join a master.csv file into the final
     master.csv file"""
 
-    def merge_csv(self, source: str, destination: str) -> pd.DataFrame:
+    @staticmethod
+    def merge_csv(source: str, destination: str) -> pd.DataFrame:
         # generate list of files to merge
         files_to_merge = glob.glob(os.path.join(source, "*csv"))
 
         # these next steps ensure we aren't merging a master.csv file with other files to make a new master.csv
-        file_to_remove = os.path.isfile(os.path.join(source,"X-Materials_master_data.csv"))
+        file_to_remove = os.path.isfile(os.path.join(source, "X-Materials_master_data.csv"))
         if file_to_remove in files_to_merge:
             files_to_merge.remove(file_to_remove)
 
@@ -27,6 +30,7 @@ class CSVMerger:
 
         # join the dataframes together with an outer join, then save it as the master.csv file
         finaldf = pd.concat(dfs, axis=1, join='outer').to_csv(os.path.join(destination, "X-Materials_master_data.csv"))
+
         return finaldf
 
 
@@ -38,7 +42,8 @@ class XLabDataEngine:
     and the engine converts these files into a single
     10xN or 12xN matrix with all data contain within."""
 
-    def set_up_headers(self, source: str, wildcard: str) -> list:
+    @staticmethod
+    def set_up_headers(source: str, wildcard: str) -> list:
         """To convert the set of Hall*txt and ICP*txt files
         located in the path into a single 10xN or 12xN dataframe,
         we need do a lot of swizzling. First, we need to establish
@@ -71,18 +76,16 @@ class XLabDataEngine:
 
             # iterate on the data in column 1 using regex findall method
             for col1 in re.findall(regex, mydata):
-
                 # for each item found, add it to header_row_content
-                header_row_content.append((col1))
+                header_row_content.append(col1)
 
         # return this list of header row content, it will be used later to create the full dataframe
         return header_row_content
 
-    def build_xlab_dataframe(self, source: str, wildcard: str) -> pd.DataFrame:
-        # functionally very similar and code not very DRY which hurts my soul a bit, but again, for time's sake
-        # get all the files in the path -> this really should be it's own class method since it's exactly
-        # the same as above
-        # path = source + wildcard
+    @staticmethod
+    def build_xlab_dataframe(source: str, wildcard: str) -> pd.DataFrame:
+
+        # functionally very similar as above and not very DRY which hurts my soul a bit
         path = os.path.join(source, wildcard)
         files_to_read = []
         for file in glob.glob(path):
@@ -107,9 +110,8 @@ class XLabDataEngine:
 
                 # use regex findall method to find matches in the data
                 for col2 in re.findall(regex, mydata):
-
                     # append list with matches
-                    list_of_data_from_column_two_for_dataframe_row.append((col2))
+                    list_of_data_from_column_two_for_dataframe_row.append(col2)
 
                 # take dataframe with headers only, and insert each line from
                 # list_of_data_from_column_two_for_dataframe_row into the dataframe
@@ -118,27 +120,34 @@ class XLabDataEngine:
         # return shiny new dataframe with all the data
         return df
 
-    def xlab_csv_file_builder(self, source: str, destination: str) -> None:
+    @staticmethod
+    def xlab_csv_file_builder(source: str, destination: str) -> None:
         """this class method  instantiates the XlabDataEngine,
         calls the build methods, converts them to tables using the
         petl python library, and then writes the tables to csv files.
         Filenames are hard-coded in. """
 
         # check to see if we have Hall*txt files in the source dir
-        if glob.glob(os.path.join(source,"*Hall*txt")):
+        if glob.glob(os.path.join(source, "*Hall*txt")):
 
             # build new XLabData dataframes with specific wildcards
             hall_data = XLabDataEngine().build_xlab_dataframe(source, "*Hall*txt")
+
             # convert dataframes to tables with petl, needed for correct data formatting
             hall_table = etl.fromdataframe(hall_data)
+
             # write tables to csv
             etl.tocsv(hall_table, os.path.join(destination, "hall_xlab.csv"))
 
         # really dislike how not DRY this is; need to refactor at some point
-        elif glob.glob(os.path.join(source,"*ICP*txt")):
+        elif glob.glob(os.path.join(source, "*ICP*txt")):
+
             icp_data = XLabDataEngine().build_xlab_dataframe(source, "*ICP*txt")
+
             icp_table = etl.fromdataframe(icp_data)
+
             etl.tocsv(icp_table, os.path.join(destination, "icp_xlab.csv"))
+
         return
 
 
@@ -146,32 +155,44 @@ class Deleter:
     """Deleter class has two methods for deleting files.
     Currently unused but we have the option if needed"""
 
-    def delete_temp_xlab_csv_files(self, source: str) -> None: #currently not using this methood
+    # currently not using this methood
+    @staticmethod
+    def delete_temp_xlab_csv_files(source: str) -> None:
+
         xlab_files = ["hall_xlab.csv", "icp_xlab.csv"]
         for file in xlab_files:
             if os.path.isfile(os.path.join(source, file)):
                 os.remove(os.path.join(source, file))
 
-    def delete_current_master_csv_file(self, source: str) -> None:
-        """look for a master csv file in the destination path
-        and remove it if it exists"""
+    @staticmethod
+    def delete_current_master_csv_file(source: str) -> None:
 
+        # define filename
         master_csv = "X-Materials_master_data.csv"
+
+        # if the file exists, remove it
         if os.path.isfile(os.path.join(source, master_csv)):
             os.remove(os.path.join(source, master_csv))
         return
 
+
 class Warnings:
     """Warning class to give useful warnings when needed"""
 
-    # attribute available for any instance to quickly throw a warning
-    warning = 'WARNING'
+    def __init__(self):
+        self.warning = 'WARNING'
 
-    def warning_if_master_in_source_path(self, source: str) -> None:
+    # attribute available for any instance to quickly throw a warning
+
+    @staticmethod
+    def warning_if_master_in_source_path(source: str) -> None:
         """look for a master csv file in the destination path
         and remove it if it exists"""
 
+        # define filename
         master_csv = "X-Materials_master_data.csv"
+
+        # if found, print a warning message
         if os.path.isfile(os.path.join(source, master_csv)):
             print("It looks like you already have a "
                   "master CSV file in your source directory.\n"
@@ -179,23 +200,34 @@ class Warnings:
             sys.exit(0)
         return
 
-    def warning_if_master_in_destination_path(self, destination: str) -> None:
+    @staticmethod
+    def warning_if_master_in_destination_path(destination: str) -> None:
         """look for a master csv file in the destination path
         and remove it if it exists"""
 
+        # define filename
         master_csv = "X-Materials_master_data.csv"
         if os.path.isfile(os.path.join(destination, master_csv)):
+
+            # if found, print a warning message, ask if overwrite is ok
             print("Master CSV file already exists in destination path.\n"
                   "OK to overwrite?\n"
                   "You can select [y]es or [n]o")
+
             affirmative = ["yes", "Yes", 'YES', 'y', 'Y']
+
             answer = input()
+
+            # if overwrite is ok, then continue back to main runner
             if answer in affirmative:
                 pass
+
+            # otherwise, get the heck out of here
             else:
                 print("Exiting without doing anything.")
                 sys.exit(0)
         return
+
 
 class Runner:
     """This is the runner class for the command line interface
@@ -203,52 +235,66 @@ class Runner:
     with conditional logic and a few loops to get input.
     Messy and should probably be broken into functions"""
 
-    def cmd_line_interface(self) -> None:
+    @staticmethod
+    def cmd_line_interface() -> None:
+
         # instantiate classes
-        myXlab_data_writer = XLabDataEngine()
-        myWarningObj = Warnings()
-        myMerger = CSVMerger()
+        my_xlab_data_writer = XLabDataEngine()
+        my_warning_obj = Warnings()
+        my_merger = Merger()
 
         print("Welcome to the Data Management System application!")
+
         print("Please enter the source path now:\n")
         source = input()
+
+        # input validation
         if not os.path.isdir(source):
             print(f"{source} is not a valid path.\n\nPlease double check your path "
                   f"and try running the app again.")
             sys.exit()
+
         print("Please enter the destination (path to save files) now:\n")
         destination = input()
+
+        # input validation
         if not os.path.isdir(destination):
             print(f"{destination} is not a valid path.\n\nPlease double check your path "
                   f"and try running the app again.")
             sys.exit()
+
+        # print source and destination to stdout
         print(f"Your source path is: \n{source}\n\n and destination path is: \n{destination}\n")
+
+        # warning if source and destination match
         if source == destination:
-            print(myWarningObj.warning)
+            print(my_warning_obj.warning)
             print("Source and destination are the same path!\n")
         print("Is this correct? Type [y]es or [n]o.")
+
         answer = input()
         affirmative = ["yes", "Yes", 'YES', 'y', 'Y']
-        negative = ["no", "No", "NO", "n", "N", "no siree"]
+
+        # conditional do stuff
         if answer in affirmative:
             try:
                 print("Generating master.csv file now...\n")
                 # generate X-lab csv files
-                myXlab_data_writer.xlab_csv_file_builder(source, destination)
+                my_xlab_data_writer.xlab_csv_file_builder(source, destination)
 
                 # throw warnings if necessary
-                myWarningObj.warning_if_master_in_source_path(source)
-                myWarningObj.warning_if_master_in_destination_path(destination)
+                my_warning_obj.warning_if_master_in_source_path(source)
+                my_warning_obj.warning_if_master_in_destination_path(destination)
 
                 # run the merger, merge *csv files in source, write output master csv to destination
-                myMerger.merge_csv(source, destination)
+                my_merger.merge_csv(source, destination)
 
                 print(f"Done.\nX-Materials_master_data.csv file "
                       f"located in: \n{destination}\nExiting. Have a nice day!")
 
             # catch both index and value errors together in this tuple since the warning is the same
             except (IndexError, ValueError):
-                print(myWarningObj.warning)
+                print(my_warning_obj.warning)
                 print(f"Cannot continue. Possibly missing data in your source path."
                       f"\nPlease double check your source path. It"
                       f" is currently set to: \n\n{source}\n")
@@ -256,7 +302,7 @@ class Runner:
 
             # warning if trying to work write files already open
             except PermissionError:
-                print(myWarningObj.warning)
+                print(my_warning_obj.warning)
                 print(f"Cannot continue.\n"
                       f"Please close all CSV and TXT files in source path, and try again.")
                 sys.exit(0)
@@ -266,24 +312,16 @@ class Runner:
 
 
 def main():
-
     # instantiate a runner
     run_the_code = Runner()
 
     # then run it!
     run_the_code.cmd_line_interface()
 
+
 if __name__ == '__main__':
     main()
 
-#C:\\Users\\matth\\Downloads\\dae-challenge
-
-#C:\\Users\\matth\\Downloads\\dae-challenge\\dae-challenge\\x-lab-data
-
-
-#destination = 'C:\\Users\\matth\\Downloads\\dae-challenge\\dae-challenge\\x-lab-data'
-
-'''source = 'C:\\Users\\matth\\Downloads\\dae-challenge'
-destination = 'C:\\Users\\matth\\Downloads\\dae-challenge'
-myXlab_data_writer = XLabDataEngine()
-myXlab_data_writer.xlab_csv_file_builder(source, destination)'''
+# example paths for testing
+# C:\\Users\\matth\\Downloads\\dae-challenge
+# C:\\Users\\matth\\Downloads\\dae-challenge\\dae-challenge\\x-lab-data
