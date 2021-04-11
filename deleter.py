57b47800c5b48fb610f387557754a9a306b7c9d4
Matthew Kinzler <matthew.kinzler@gmail.com>
2021-04-11 14:21:05 -0400

    refactor delete class

--- deleter.py
+++ deleter.py
@@ -5,8 +5,7 @@ class Deleter:
     Currently unused but we have the option if needed"""
 
     # currently not using this method
-    @staticmethod
-    def delete_temp_xlab_csv_files(source: str) -> None:
+    def delete_temp_xlab_csv_files(self, source: str) -> None:
 
         xlab_files = ["hall_xlab.csv", "icp_xlab.csv"]
         for file in xlab_files:
@@ -15,8 +14,7 @@ class Deleter:
         return
 
     # currently not using this method
-    @staticmethod
-    def delete_current_master_csv_file(source: str) -> None:
+    def delete_current_master_csv_file(self, source: str) -> None:
 
         # define filename
         master_csv = "X-Materials_master_data.csv"
